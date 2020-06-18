24177c084066b24a8bfcfd50ccbd1b19844814c4
LuizPPA <luizphilippep@gmail.com>
2020-06-18 03:23:49 -0300

    Hotfix

--- routes/server.js
+++ routes/server.js
@@ -5,7 +5,7 @@ var router = express.Router()
 
 router.post('/open', server_controller.open)
 router.post('/close', server_controller.close)
-router.get('/reboot', server_controller.reboot)
+router.post('/reboot', server_controller.reboot)
 router.get('/status', server_controller.status)
 
 module.exports = router;
